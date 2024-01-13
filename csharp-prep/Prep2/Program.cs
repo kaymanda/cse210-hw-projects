083e1f6e30a50dda6524a1f6611748b75dedea0b
kaymanda <mandaks86@gmail.com>
2024-01-12 23:38:29 -0700

    grades and percentage with message.

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,41 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("Please enter your grade percentage? ");
+        string response = Console.ReadLine();
+        int percentage = int.Parse(response);
+        string letterGrade = "";
+
+        if (percentage >= 90)
+        {
+            letterGrade = "A";
+        }
+        else if (percentage >= 80)
+        {
+            letterGrade = "B";
+        }
+        else if (percentage >= 70)
+        {
+            letterGrade = "C";
+        }
+        else if (percentage >= 60)
+        {
+            letterGrade = "D";
+        }
+        else 
+        {
+            letterGrade = "F";
+        }
+
+        Console.WriteLine($"Your letter grade is: {letterGrade}");
+
+        if (percentage >= 70)
+        {
+            Console.WriteLine("Yay! You Passed!");
+        }
+        else
+        {
+            Console.WriteLine("All the best luck next time.");
+        }
     }
 }
