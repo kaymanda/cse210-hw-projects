a093741c3aa9b4b669847cb6ad1219a8375af604
kaymanda <mandaks86@gmail.com>
2024-01-13 01:08:40 -0700

    Step 3 random generator

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,11 +4,15 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.Write("What is the magic number? ");
-        int magicNumber = int.Parse(Console.ReadLine());
+        //Console.Write("What is the magic number? ");
+        //int magicNumber = int.Parse(Console.ReadLine());
+        //add random generator
+        Random randomGenerator = new Random();
+        int magicNumber = randomGenerator.Next(1, 101);
 
         int userGuess = -1;
-
+        
+        //while loop
         while (userGuess != magicNumber)
         {
             Console.Write("What is your guess? ");
