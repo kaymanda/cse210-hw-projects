dbff31a7e66f8a0bba09baab050d60dd1c7c3bcf
kaymanda <mandaks86@gmail.com>
2024-01-13 16:15:27 -0700

    functions added, name, number, squared

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -4,6 +4,46 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep5 World!");
+        DisplayWelcome();
+        
+        string usersName = PromptUserName();
+        int usersNumber = PromptUserNumber();
+
+        int squaredNumber = SquareNumber(usersNumber);
+
+        DisplayResult(usersName, squaredNumber);   
+    }
+
+    static void DisplayWelcome()
+    {
+        Console.WriteLine("Welcome to this program!");
+    }
+
+    static string PromptUserName()
+    {
+        Console.Write("Please enter your name: ");
+        string yourName = Console.ReadLine();
+
+        return yourName;
+    }
+
+    static int PromptUserNumber()
+    {
+        Console.Write("Please enter your favorite number: ");
+        int yourNumber = int.Parse(Console.ReadLine());
+
+        return yourNumber;
+    }
+
+    static int SquareNumber(int yourNumber)
+    {
+        int squared = yourNumber * yourNumber;
+
+        return squared;
+    }
+
+    static void DisplayResult(string yourName, int squared)
+    {
+        Console.WriteLine($"{yourName}, the square of your number is {squared}");
     }
 }
