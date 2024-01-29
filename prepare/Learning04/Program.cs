2f2287171f94b86af87c8e3058d605b232780de1
kaymanda <mandaks86@gmail.com>
2024-01-29 11:34:23 -0700

    using Inheritance

--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -4,6 +4,24 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning04 World!");
+        //base class
+        Assignment assign1 = new Assignment("Samuel Bennett", "Multiplication");
+        Console.WriteLine(assign1.GetSummary());
+        Console.WriteLine();
+        
+        //MathAssignment class
+        MathAssignment assign2 = new MathAssignment("Roberto Rodriguez", "Fractions", "7.3", "8-19");
+        Console.WriteLine(assign2.GetSummary());
+        Console.WriteLine(assign2.GetHomeworkList());
+        Console.WriteLine();
+
+        //WritingAssignment class
+        WritingAssignment assign3 = new WritingAssignment("Mary Waters","European History", "The Cause of World War II");
+        Console.WriteLine(assign3.GetSummary());
+        Console.WriteLine(assign3.GetWritingInformation());
+        Console.WriteLine();
+
+
+
     }
 }
