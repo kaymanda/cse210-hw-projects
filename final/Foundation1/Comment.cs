3898efd5255442349b359f2f22b088cdea3ad770
kaymanda <mandaks86@gmail.com>
2024-02-17 10:24:50 -0700

    just a start

---
+++ final/Foundation1/Comment.cs
@@ -0,0 +1,12 @@
+using System;
+
+public class AddComments
+{
+    public string _name;
+    public string _text;
+
+    public void Display()
+    {
+        
+    }
+}
---
+++ final/Foundation1/Video.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Transactions;
+
+public class Video
+{
+    public string _title;
+    public string _streamer;
+    public string _length;
+    
+   // public List<Comment> _comments = new List<Comment>();
+
+
+    public void AddComments()
+    {
+
+    }
+    public int GetNumComment()
+    {
+        return 0;
+    }
+    public void Display()
+    {
+
+    }
+
+
+}
